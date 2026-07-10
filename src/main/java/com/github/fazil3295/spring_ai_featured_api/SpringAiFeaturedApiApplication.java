157e083ed9210b87a18904fd13ab166b345ed2bc
fazil3295 <mohammedfazil.kadherbasha@gmail.com>
2026-07-10 21:49:47 +0530

    Project initialization with required dependencies config

---
+++ src/main/java/com/github/fazil3295/spring_ai_featured_api/SpringAiFeaturedApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.fazil3295.spring_ai_featured_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiFeaturedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiFeaturedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/fazil3295/spring_ai_featured_api/SpringAiFeaturedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fazil3295.spring_ai_featured_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiFeaturedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
